d15357de5e8775d326adb8d6538e75a2762b59b2
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-03-04 23:37:37 -0600

    FirstDemo project added

---
+++ FirstDemo/FirstDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace FirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("***********************************************");
+            WriteLine("Este programa resuelve la ecuación ax^2+bx+c=0");
+            WriteLine("***********************************************");
+            WriteLine("\nIntroduce los valores");
+
+            Write("a: ");
+            int a = Convert.ToInt32(ReadLine());
+            Write("b: ");
+            int b = Convert.ToInt32(ReadLine());
+            Write("c: ");
+            int c = Convert.ToInt32(ReadLine());
+
+            if ((a == 0) && (b == 0))
+            {
+                WriteLine("\n¡No es una ecuación válida!");
+            }
+            else if ((a==0) && (b!=0)) // (b!=0) no es necesario
+            {
+                WriteLine("\n¡Es una ecuación lineal!");
+
+                double x = -c / (double)b; // explicit conversión - casting
+                WriteLine($"La solución es: x = {x}");
+            }
+        }
+    }
+}
