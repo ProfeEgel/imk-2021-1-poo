6839bbad98bfb245373d1e287dc50161b3279433
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-04-13 21:34:26 -0500

    Update Program.cs

--- POOBasics/POOBasics/Program.cs
+++ POOBasics/POOBasics/Program.cs
@@ -86,21 +86,77 @@ namespace POOBasics
         }
 
         //public int CalculateVolume() => width * length * height;
+
+        // empty constructor (default)
+        // constructor sin parámetros
+        public Box()
+        {
+            width = 1;
+            length = 1;
+            height = 1;
+        }
+
+        public Box(int size)
+        {
+            width = size;
+            length = size;
+            height = size;
+        }
+
+        public Box(int width, int length, int height)
+        {
+            this.width = width;
+            this.length = length;
+            this.height = height;
+        }
+
+        public Box(Box box)
+        {
+            width = box.width;
+            length = box.length;
+            height = box.height;
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Box b1 = new Box();
-            b1.Width = 3; // el width de la caja a la que hace referencia b1
-            b1.Length = 2;
-            b1.Height = 1;
+            //Box[] boxes = new Box[5];
+            Box[] boxes =
+            {
+                new Box(),
+                new Box(1, 2, 3),
+                new Box(3, 2, 1),
+                new Box(),
+                new Box(5),
+                new Box(2)
+            };
 
-            Box b2 = new Box();
-            b2.Width = 6;
-            b2.Length = b1.Length;
-            b2.Height = b1.Height;
+            foreach (Box box in boxes)
+            {
+                WriteLine($"\nVolumen = {box.Volume}");
+                WriteLine($"->({box.Width},{box.Length},{box.Height})");
+            }
+
+            //for (int i = 0; i < boxes.Length; ++i)
+            //{
+            //    WriteLine($"\nVolumen = {boxes[i].Volume}");
+            //    WriteLine($"->({boxes[i].Width},{boxes[i].Length},{boxes[i].Height})");
+            //}
+        }
+
+        static void Main2(string[] args)
+        {
+            Box b1 = new Box(3, 2, 1);
+            //b1.Width = 3; // el width de la caja a la que hace referencia b1
+            //b1.Length = 2;
+            //b1.Height = 1;
+
+            Box b2 = new Box(b1) { Width = 6 };
+            //b2.Width = 6;
+            //b2.Length = b1.Length;
+            //b2.Height = b1.Height;
 
             WriteLine($"\nVolumen = {b1.Volume}");
             WriteLine($"b1->({b1.Width},{b1.Length},{b1.Height})");
