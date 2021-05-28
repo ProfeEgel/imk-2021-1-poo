cc0338b3fa73a99da7d8cf2b8544fc8c4f158fbc
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-05-28 02:07:07 -0500

    FormsDemo added

---
+++ FormsDemo/FormsDemo/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace FormsDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbTest = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTest.Location = new System.Drawing.Point(428, 17);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(87, 33);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Prueba:";
+            // 
+            // tbTest
+            // 
+            this.tbTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTest.Location = new System.Drawing.Point(97, 20);
+            this.tbTest.Name = "tbTest";
+            this.tbTest.Size = new System.Drawing.Size(316, 26);
+            this.tbTest.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 287);
+            this.Controls.Add(this.tbTest);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbTest;
+    }
+}
+
---
+++ FormsDemo/FormsDemo/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsDemo
+{
+    public partial class Form1 : Form
+    {
+        private int contador = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            tbTest.Text = "Contador de clicks: 0";
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            tbTest.Text = $"Contador de clicks: {++contador}";
+
+            //tbTest.Text = "¡¡¡Test Click!!!";
+            // MessageBox.Show("Hola");
+        }
+    }
+}
