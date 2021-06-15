8e3902c89c37984baece1145cbc3dff6503f773a
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-06-15 14:39:30 -0500

    DataListsDemo updated

--- DataListsDemo/DataListsDemo/MainForm.Designer.cs
+++ DataListsDemo/DataListsDemo/MainForm.Designer.cs
@@ -39,13 +39,24 @@ namespace DataListsDemo
             this.tbValue = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
+            this.lstUsers = new System.Windows.Forms.ListBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pbInstall = new System.Windows.Forms.ProgressBar();
+            this.nudProgress = new System.Windows.Forms.NumericUpDown();
+            this.pbStatus = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudProgress)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(23, 40);
+            this.label1.Location = new System.Drawing.Point(24, 64);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(76, 20);
             this.label1.TabIndex = 0;
@@ -56,7 +67,7 @@ namespace DataListsDemo
             this.cmbUsers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbUsers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbUsers.FormattingEnabled = true;
-            this.cmbUsers.Location = new System.Drawing.Point(105, 37);
+            this.cmbUsers.Location = new System.Drawing.Point(106, 61);
             this.cmbUsers.Name = "cmbUsers";
             this.cmbUsers.Size = new System.Drawing.Size(250, 28);
             this.cmbUsers.TabIndex = 1;
@@ -66,7 +77,7 @@ namespace DataListsDemo
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(385, 40);
+            this.label2.Location = new System.Drawing.Point(386, 64);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(115, 20);
             this.label2.TabIndex = 0;
@@ -76,7 +87,7 @@ namespace DataListsDemo
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(392, 72);
+            this.label3.Location = new System.Drawing.Point(393, 96);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(108, 20);
             this.label3.TabIndex = 0;
@@ -86,7 +97,7 @@ namespace DataListsDemo
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(383, 104);
+            this.label4.Location = new System.Drawing.Point(384, 128);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(117, 20);
             this.label4.TabIndex = 0;
@@ -95,7 +106,7 @@ namespace DataListsDemo
             // tbIndex
             // 
             this.tbIndex.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbIndex.Location = new System.Drawing.Point(506, 37);
+            this.tbIndex.Location = new System.Drawing.Point(507, 61);
             this.tbIndex.Name = "tbIndex";
             this.tbIndex.ReadOnly = true;
             this.tbIndex.Size = new System.Drawing.Size(229, 26);
@@ -104,7 +115,7 @@ namespace DataListsDemo
             // tbItem
             // 
             this.tbItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbItem.Location = new System.Drawing.Point(506, 69);
+            this.tbItem.Location = new System.Drawing.Point(507, 93);
             this.tbItem.Name = "tbItem";
             this.tbItem.ReadOnly = true;
             this.tbItem.Size = new System.Drawing.Size(229, 26);
@@ -113,7 +124,7 @@ namespace DataListsDemo
             // tbValue
             // 
             this.tbValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbValue.Location = new System.Drawing.Point(506, 101);
+            this.tbValue.Location = new System.Drawing.Point(507, 125);
             this.tbValue.Name = "tbValue";
             this.tbValue.ReadOnly = true;
             this.tbValue.Size = new System.Drawing.Size(229, 26);
@@ -122,7 +133,7 @@ namespace DataListsDemo
             // btnAdd
             // 
             this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.Location = new System.Drawing.Point(506, 157);
+            this.btnAdd.Location = new System.Drawing.Point(507, 181);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(101, 37);
             this.btnAdd.TabIndex = 3;
@@ -134,7 +145,7 @@ namespace DataListsDemo
             // 
             this.btnDelete.Enabled = false;
             this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDelete.Location = new System.Drawing.Point(506, 200);
+            this.btnDelete.Location = new System.Drawing.Point(507, 224);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(101, 37);
             this.btnDelete.TabIndex = 3;
@@ -142,11 +153,93 @@ namespace DataListsDemo
             this.btnDelete.UseVisualStyleBackColor = true;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
+            // lstUsers
+            // 
+            this.lstUsers.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstUsers.FormattingEnabled = true;
+            this.lstUsers.ItemHeight = 18;
+            this.lstUsers.Location = new System.Drawing.Point(106, 107);
+            this.lstUsers.Name = "lstUsers";
+            this.lstUsers.Size = new System.Drawing.Size(250, 112);
+            this.lstUsers.TabIndex = 4;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.Location = new System.Drawing.Point(642, 181);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(94, 38);
+            this.btnShow.TabIndex = 5;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(767, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsStatus,
+            this.pbStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 354);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(767, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsStatus
+            // 
+            this.tsStatus.Name = "tsStatus";
+            this.tsStatus.Size = new System.Drawing.Size(39, 17);
+            this.tsStatus.Text = "Ready";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 248);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Instalando:";
+            // 
+            // pbInstall
+            // 
+            this.pbInstall.Location = new System.Drawing.Point(106, 248);
+            this.pbInstall.Name = "pbInstall";
+            this.pbInstall.Size = new System.Drawing.Size(250, 23);
+            this.pbInstall.TabIndex = 8;
+            // 
+            // nudProgress
+            // 
+            this.nudProgress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudProgress.Location = new System.Drawing.Point(269, 288);
+            this.nudProgress.Name = "nudProgress";
+            this.nudProgress.Size = new System.Drawing.Size(87, 26);
+            this.nudProgress.TabIndex = 9;
+            this.nudProgress.ValueChanged += new System.EventHandler(this.nudProgress_ValueChanged);
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(100, 16);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(766, 306);
+            this.ClientSize = new System.Drawing.Size(767, 376);
+            this.Controls.Add(this.nudProgress);
+            this.Controls.Add(this.pbInstall);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.lstUsers);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.tbValue);
@@ -156,12 +249,18 @@ namespace DataListsDemo
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lists Demo App v1.0";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudProgress)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -179,6 +278,15 @@ namespace DataListsDemo
         private System.Windows.Forms.TextBox tbValue;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ListBox lstUsers;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsStatus;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar pbInstall;
+        private System.Windows.Forms.NumericUpDown nudProgress;
+        private System.Windows.Forms.ToolStripProgressBar pbStatus;
     }
 }
 
--- DataListsDemo/DataListsDemo/MainForm.cs
+++ DataListsDemo/DataListsDemo/MainForm.cs
@@ -12,6 +12,9 @@ namespace DataListsDemo
 {
     public partial class MainForm : Form
     {
+        private List<string> items = new List<string>();
+        private List<User> users = new List<User>();
+
         public MainForm()
         {
             InitializeComponent();
@@ -33,21 +36,58 @@ namespace DataListsDemo
             // SelectedIndex - Ok
             // SelectedItem  - Ok
             // SelectedValue - N/A
+            //cmbUsers.DisplayMember = "FullName";
+            //cmbUsers.Items.Add(new User("Jesús", "Bermudez", 25));
+            //cmbUsers.Items.Add(new User("Silvia", "Huerta", 18));
+            //cmbUsers.Items.Add(new User("Manuel", "Ferrer", 36));
+            //cmbUsers.Items.Add(new User("Francisca", "Jurado", 42));
+            //cmbUsers.Items.Add(new User("Rubén", "Escribano", 27));
+            //cmbUsers.Items.Add(new User("Laia", "García", 74));
+            //cmbUsers.Items.Add(new User("Oscar", "Torrente", 53));
+            //cmbUsers.SelectedIndex = 0;
+
+            // #3 - DataSource - Strings (lista)
+            // SelectedIndex - Ok
+            // SelectedItem  - Ok (string)
+            // SelectedValue - Ok (string)
+            // Principal uso: datos no serán editados
+            //items.Add("Bermudez, Jesús");
+            //items.Add("Huerta, Silvia");
+            //items.Add("Ferrer, Manuel");
+            //items.Add("Jurado, Francisca");
+            //items.Add("Escribano, Rubén");
+            //items.Add("García, Laia");
+            //items.Add("Torrente, Oscar");
+            
+            //cmbUsers.DataSource = items;
+
+            // 4# - DataSource - Objects
+            // SelectedIndex - Ok
+            // SelectedItem  - Ok
+            // SelectedValue - Ok (ValueMember habilitado)
+            users.Add(new User("Jesús", "Bermudez", 25));
+            users.Add(new User("Silvia", "Huerta", 18));
+            users.Add(new User("Manuel", "Ferrer", 36));
+            users.Add(new User("Francisca", "Jurado", 42));
+            users.Add(new User("Rubén", "Escribano", 27));
+            users.Add(new User("Laia", "García", 74));
+            users.Add(new User("Oscar", "Torrente", 53));
+
             cmbUsers.DisplayMember = "FullName";
-            cmbUsers.Items.Add(new User("Jesús", "Bermudez", 25));
-            cmbUsers.Items.Add(new User("Silvia", "Huerta", 18));
-            cmbUsers.Items.Add(new User("Manuel", "Ferrer", 36));
-            cmbUsers.Items.Add(new User("Francisca", "Jurado", 42));
-            cmbUsers.Items.Add(new User("Rubén", "Escribano", 27));
-            cmbUsers.Items.Add(new User("Laia", "García", 74));
-            cmbUsers.Items.Add(new User("Oscar", "Torrente", 53));
-            cmbUsers.SelectedIndex = 0;
+            cmbUsers.ValueMember = "Age";
+            cmbUsers.DataSource = users;
+
+            lstUsers.DisplayMember = "FullName";
+            lstUsers.ValueMember = "Age";
+            lstUsers.DataSource = users;
         }
 
         private void cmbUsers_SelectedIndexChanged(object sender, EventArgs e)
         {
             // #1 - Items - Strings
             // #2 - Items - Objects
+            // #3 - DataSource - Strings
+            // #4 - DataSource - Objects
             btnDelete.Enabled = true;
 
             tbIndex.Text = $"{cmbUsers.SelectedIndex}";
@@ -61,19 +101,73 @@ namespace DataListsDemo
             //cmbUsers.Items.Add("Agregado, Usuario");
 
             // #2 - Items - Objects
-            cmbUsers.Items.Add(new User("Usuario", "Agregado", 33));
+            //cmbUsers.Items.Add(new User("Usuario", "Agregado", 33));
+
+            // #3 - DataSource - Strings
+            //items.Add("Agregado, Usuario");
+            //cmbUsers.DataSource = null;
+            //cmbUsers.DataSource = items;
+
+            // #4 - DataSource - Objects
+            users.Add(new User("Usuario", "Agregado", 33));
+            cmbUsers.DataSource = null;
+            cmbUsers.DisplayMember = "FullName";
+            cmbUsers.ValueMember = "Age";
+            cmbUsers.DataSource = users;
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
             // #1 - Items - Strings
             // #2 - Items - Objects
-            cmbUsers.Items.RemoveAt(cmbUsers.SelectedIndex);
+            //cmbUsers.Items.RemoveAt(cmbUsers.SelectedIndex);
+
+            // #3 - DataSource - Strings // ejemplo, no funciona bien
+            //items.RemoveAt(cmbUsers.SelectedIndex);
+            //cmbUsers.DataSource = null;
+            //cmbUsers.DataSource = items;
+
+            // #4 - DataSource - Objects // ejemplo, no funciona bien
+            users.RemoveAt(cmbUsers.SelectedIndex);
+            cmbUsers.DataSource = null;
+            cmbUsers.DisplayMember = "FullName";
+            cmbUsers.ValueMember = "Age";
+            cmbUsers.DataSource = users;
 
             btnDelete.Enabled = false;
             tbIndex.Clear();
             tbItem.Clear();
             tbValue.Clear();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            switch (MessageBox.Show("¿Confirmas borrar el elemento seleccionado?",
+                                    "Aviso", MessageBoxButtons.YesNoCancel,
+                                    MessageBoxIcon.Warning))
+            {
+                case DialogResult.Yes:
+                    //MessageBox.Show("Seleccionaste: SI");
+                    tsStatus.Text = "Seleccionaste: SI";
+                    break;
+
+                case DialogResult.No:
+                    //MessageBox.Show("Seleccionaste: NO");
+                    tsStatus.Text = "Seleccionaste: NO";
+                    break;
+
+                default:
+                case DialogResult.Cancel:
+                    //MessageBox.Show("Seleccionaste: CANCELAR o cerraste la ventana");
+                    tsStatus.Text = "Seleccionaste: CANCELAR o cerraste la ventana";
+                    break;
+            }
+        }
+
+        private void nudProgress_ValueChanged(object sender, EventArgs e)
+        {
+            pbInstall.Value = (int)nudProgress.Value;
+            pbStatus.Value = (int)nudProgress.Value;
         }
     }
 }
